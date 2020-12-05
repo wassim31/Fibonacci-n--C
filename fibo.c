1da8ab806eaff9844d83a9295258c52ebe3fd528
wassim31 <wassimbs088@gmail.com>
2020-12-05 19:59:49 +0100

    logic fault

--- fibo.c
+++ fibo.c
@@ -3,11 +3,11 @@
 
 int main (int argc , char **argv[])
 {
-    int n, Un_1 = 1, Un_2 = 1, i; // F0 = 1 / F1 = 1 / 1,1,2,3,5,8,13,21,34,55...
+    int n, Un_1 = 0, Un_2 = 1, i; // F0 = 1 / F1 = 1 / 1,1,2,3,5,8,13,21,34,55...
     scanf("%d", &n);
     if(n == 0)
     {
-        Un_1 = 1;
+        Un_1 = 0;
         printf("%d\n", Un_1);
     }
     else if(n == 1)
